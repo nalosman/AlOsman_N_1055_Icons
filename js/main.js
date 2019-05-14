5d33f6dbb1f12c85987601169f002343732b92c6
nour <nour@Nours-MBP.fc.ca>
2019-05-14 17:59:29 -0400

    added js interactivity ot icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 (() => {
   //this is a self invoking function
-
   console.log('my JS is workin!!!');
+
+  // select elements with javascript
+
+  // seeting up a variable using the let keyword,
+  // and using a css selector to find the element we want to querySelector
+
+  // let stores the reference to element as a variable (in memory)
+  let svgGraphic = document.querySelector("#badgeSVG");
+
+  // functions are resuable pieces of CODE
+  // that you can run anytime
+  function logMyId() {
+    console.log(this.id);
+
+    this.style.opacity = 0.5;
+  }
+
+  svgGraphic.addEventListener("click", logMyId)
 })();
